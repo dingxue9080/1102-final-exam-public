66cf89b19fbdcae2e3f7046952595692bec11e7a
dingxue9080 <106822608+dingxue9080@users.noreply.github.com>
2022-12-27 16:32:04 +0800

    Update p09.py

--- exam/p/p09.py
+++ exam/p/p09.py
@@ -1,6 +1,15 @@
 def p09(citations=[]):
     h=None
     # ↓程式區域↓
-
+    citations.sort()
+    citations.reverse()
+    for i in range(0,len(citations)):
+        count=0
+        h=citations[i]
+        for j in range(0,len(citations)):
+            if(citations[j]>=h):
+                count+=1
+        if(count>=h):
+            break
     # ↑程式區域↑
     return h
