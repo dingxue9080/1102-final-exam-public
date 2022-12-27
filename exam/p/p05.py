c74a23b90ee57572662582a111dd917dd7371c36
dingxue9080 <106822608+dingxue9080@users.noreply.github.com>
2022-12-27 16:26:35 +0800

    Update p05.py

--- exam/p/p05.py
+++ exam/p/p05.py
@@ -1,6 +1,14 @@
 def p05(l1=[1,4,3,2,5,2], x=3):
     output_list=None
     # ↓程式區域↓
-
+    listsmall=[]
+    listbig=[]
+    for i in range(0,len(list)):
+        if(list[i]<num):
+            listsmall.append(list[i])
+        elif(list[i]>=num):
+            listbig.append(list[i])
+    listsmall.sort()
+    output_list=listsmall+listbig
     # ↑程式區域↑
     return output_list
